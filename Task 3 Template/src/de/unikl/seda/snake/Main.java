53e29b5bafd8d266037a79fdeba4c95bce74936e
Merveille <mafo@rhrk.uni-kl.de>
2020-12-08 21:03:00 +0100

    Game Menu Overview modifications

--- Task 3 Template/src/de/unikl/seda/snake/Main.java
+++ Task 3 Template/src/de/unikl/seda/snake/Main.java
@@ -15,21 +15,29 @@ public class Main {
         System.out.println("G-Snake");
         System.out.println("²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²\n");
 
-        System.out.println("-------------------------------------------");
+        System.out.println("-------------------------------------------\n");
+        System.out.println("Game Menu Overview\n");
+
         System.out.println("[1] Start");
-        System.out.println("[0] Exit\n");
+        System.out.println("[2] Settings");
+        System.out.println("[3] Exit\n");
         System.out.print("> Select: ");
 
         int userInput = s.nextInt();
         switch(userInput) {
-            case 1: startGame(snakeGameSettings);
-            case 0: break;
+            case 1:
+                startGame(snakeGameSettings);
+                break;
+            case 2:
+                gameSettings(snakeGameSettings);
+                break;
+            case 3: break;
         }
     }
 
     private static void startGame(SnakeGameSettings snakeGameSettings) {
 
-        System.out.print("Input player name: ");
+        /*System.out.print("Input player name: ");
         String playerName = s.next();
         snakeGameSettings.setPlayerName(playerName);
 
@@ -37,7 +45,7 @@ public class Main {
         int pixel = s.nextInt();
 
         // set the pixel for the snakeGameSettings object
-        snakeGameSettings.setSquareSize(pixel);
+        snakeGameSettings.setSquareSize(pixel);*/
 
         System.out.println("Game Level Selection: ");
         System.out.println("[1]: Level 1");
@@ -57,25 +65,42 @@ public class Main {
         }
 
         // Create game instance
+        startGameInstance(snakeGameSettings);
+    }
+    private static void gameSettings(SnakeGameSettings snakeGameSettings){
+
+        System.out.println("[1] Square size");
+        System.out.println("[2] Player name");
+        System.out.println("[3] Game speed\n");
+        System.out.print("> Select: ");
+
+        int userInput = s.nextInt();
+        switch (userInput) {
+            case 1:
+                // Change Square size
+                System.out.print("Enter the square size: ");
+                int squareSize = s.nextInt();
+                snakeGameSettings.setSquareSize(squareSize);
+            case 2:
+                // Change player name
+                System.out.print("Enter your new player name: ");
+                String playerName = s.next();
+                snakeGameSettings.setPlayerName(playerName);
+            case 3:
+                // Change game speed
+                System.out.print("Enter the new game speed: ");
+                int gameSpeed = s.nextInt();
+                snakeGameSettings.setGameSpeed(gameSpeed);
+        }
+        startGameInstance(snakeGameSettings);
+
+    }
+
+    private static void startGameInstance(SnakeGameSettings snakeGameSettings) {
+        // Create game instance
         SnakeGameEnvironment game = new SnakeGameEnvironment(snakeGameSettings);
         // Start game session
         GuiContainer.show("Snake", game);
-    }
-    private static void gameSettings(SnakeGameSettings snakeGameSettings){
-        // Change player name
-        System.out.print("Enter your new player name: ");
-        String playerName = s.next();
-        snakeGameSettings.setPlayerName(playerName);
-
-        // Change Square size
-        System.out.print("Enter the square size: ");
-        int squareSize = s.nextInt();
-        snakeGameSettings.setSquareSize(squareSize);
-
-        // Change game speed
-        System.out.print("Enter the new game speed: ");
-        int gameSpeed = s.nextInt();
-        snakeGameSettings.setGameSpeed(gameSpeed);
     }
 }
 
